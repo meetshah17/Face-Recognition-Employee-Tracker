60d42f9faa7088f4acd93bc891b50573d4a18e2f
DFA23SCM65A2 <dasodariya@hawk.iit.edu>
2024-03-04 14:27:22 -0600

    detect everyone's faces which comes in cameraframe

--- out_window.py
+++ out_window.py
@@ -1,4 +1,3 @@
-
 from PyQt5.QtGui import QImage, QPixmap
 from PyQt5.uic import loadUi
 from PyQt5.QtCore import pyqtSlot, QTimer
@@ -15,22 +14,29 @@ class Ui_OutputDialog(QDialog):
     @pyqtSlot()
     def startVideo(self, camera_name):
         self.capture = cv2.VideoCapture(int(camera_name))
-        self.timer = QTimer(self)  # Create Timer
-        
-        self.timer.timeout.connect(self.update_frame)  # Connect timeout to the output function
-        self.timer.start(10)  # emit the timeout() signal at x=40ms
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.update_frame)
+        self.timer.start(10)
 
     def update_frame(self):
         ret, self.image = self.capture.read()
+        self.image = self.detect_faces(self.image)
         self.displayImage(self.image, 1)
 
+    def detect_faces(self, image):
+        face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')      # Load the pre-trained face detection model
+
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)   # Convert the image to grayscale for face detection
+
+        faces = face_cascade.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))      # Detect faces in the image
+
+        for (x, y, w, h) in faces:
+            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)         # Draw rectangles around the detected faces
+
+        return image
+
     def displayImage(self, image, window=1):
-        
         image = cv2.resize(image, (640, 480))
-        try:
-            image = self.face_rec_(image)
-        except Exception as e:
-            print(e)
         qformat = QImage.Format_Indexed8
         if len(image.shape) == 3:
             if image.shape[2] == 4:
